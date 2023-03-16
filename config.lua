f63eb611b0ceff544ec1b044190dea7a7bdbe1c0
Alessandro Alterno <28725795+lexinor@users.noreply.github.com>
2023-03-16 09:21:01 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@
 
 Config = {}
 
-Config.checkForUpdates = true -- Check for Updates?
+Config.checkForUpdates = false -- Check for Updates?
 
 Config.Elevators = {
     PillboxElevatorNorth = { -- Elevator name(Doesn't show it's just to name table)
@@ -23,14 +23,39 @@ Config.Elevators = {
             },
         },
         [2] = {
-            coords = vec3(344.31, -586.12, 28.79), -- Coords, if you're new; last number is heading
+            coords = vec3(344.31, -586.12, 28.79),
             heading = 252.84,
             title = 'Floor 1',
             description = 'Lower Floor',
             target = {
-            width = 5,
-            length = 4
+                width = 5,
+                length = 4,
             } -- Example without group
+        },
+    },
+    VespucciPD = { -- Elevator name(Doesn't show it's just to name table)
+        [1] = {
+            coords = vec3(332.37, -595.56, 43.28),
+            heading = 70.65, -- Heading of how will spawn on floor
+            title = 'Floor 2', -- Title 
+            description = 'Main Floor', -- Description
+            target = { -- Target length/width
+                width = 5,
+                length = 4
+            },
+            groups = {-- Job locks
+                'police',
+            },
+        },
+        [2] = {
+            coords = vec3(344.31, -586.12, 28.79),
+            heading = 252.84,
+            title = 'Floor 1',
+            description = 'Lower Floor',
+            target = {
+                width = 5,
+                length = 4,
+            }
         },
     },
 }
